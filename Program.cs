c8764d38143027465dc357b306d8346abb3568ce
rx570rohit <rohit.soni2018@vitbhopal.ac.in>
2022-05-13 22:16:20 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RegX
+{
+    class Program
+    {
+        public static void Main(String []arg)
+        {
+            User us1 =new User();
+           Console.WriteLine( us1.validateFirstName("Rohit"));
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RegX
+{
+    internal class User
+    {
+        public static String FirstName = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public bool validateFirstName(String fname)
+        {
+            return Regex.IsMatch(fname, FirstName);
+        }
+
+    }
+}
